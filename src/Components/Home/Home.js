2dd03c9f1a85d09917919b211d634537b37c2ba9
Sivaprakash Desingu <10977339+sivaprakashDesingu@users.noreply.github.com>
2019-05-20 20:58:53 +0530

    Add files via upload

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,15 +1,42 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './home.css';
 
 
 class Home extends Component {
-    render() {
-      return (
-        <div className="Home">
-          <p>Home Page</p>
-        </div>
-      );
+  constructor(props) {
+    super(props);
+    //this.onChangeHostName = this.onChangeHostName.bind(this);
+
+    this.state = {
+      name: 'siva',
+      sn: 'prakash'
     }
   }
-  
-  export default Home;
+
+  componentDidMount() {
+    const serverport = {
+      name: this.state.name,
+      port: this.state.sn
+    }
+    http://localhost:8000
+
+    axios.post('http://localhost:8000/test', serverport)
+    .then(response => {
+      console.log(response.data)
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+  }
+
+  render() {
+    return (
+      <div className="Home">
+        <p>Home Page</p>
+      </div>
+    );
+  }
+}
+
+export default Home;
